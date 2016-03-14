7fa5e09f172a4e3680560a9b42f45b114e11e477
khaneshir <khanehshir12@gmail.com>
2016-03-14 22:21:07 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {138583448,147368581,tonumber(our_id)},--Sudo users
+    sudo_users = {138583448,147368581,},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
